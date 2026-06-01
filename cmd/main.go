f6cb7ce791585d354ccc3d85cb5aaa650527e562
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-06-01 21:39:28 +0200

    set up basic tcp listener

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	PORT = "5555"
+	TYPE = "tcp"
+)
 
 func main() {
-	fmt.Println("hello world")
+	l, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("failed to listen to port prev: %s", err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Failed to accept request prev: %s", err)
+		}
+		go func() {
+			buff := make([]byte, 1024)
+			n, err := conn.Read(buff)
+			if err != nil {
+				log.Fatalf("Failed to read from request prev: err")
+			}
+			if n != 0 {
+				fmt.Printf("success here is the content: %s", string(buff))
+			}
+			defer conn.Close()
+		}()
+	}
 }
