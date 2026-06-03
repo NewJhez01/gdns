563a5879be1984c85a57b650f8956ca12bec4d18
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-06-03 21:55:22 +0200

    finish head line parsing

--- internal/infrastructure/parser/header_parser.go
+++ internal/infrastructure/parser/header_parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 )
@@ -51,11 +52,7 @@ type Header struct {
 }
 
 func CreateHeader() *Header {
-	// quote from rfc 1035
-	// Reserved for future use.  Must be zero in all queries and responses.
-	return &Header{
-		Z: 0,
-	}
+	return &Header{}
 }
 
 func (h *Header) Parse(r io.Reader) error {
@@ -82,10 +79,15 @@ func (h *Header) Parse(r io.Reader) error {
 func (h *Header) parseMisc(b []byte) error {
 	bits := binary.BigEndian.Uint16(b)
 	h.Qr = QR((bits >> 15) & 0x1)
-	h.Aa = (bits>>10)&0x1 == 0x1
-	h.Tc = (bits>>9)&0x1 == 0x1
-	h.Rd = (bits>>8)&0x1 == 0x1
-	h.Ra = (bits>>7)&0x1 == 0x1
+	h.Aa = (bits>>10)&0x1 == 1
+	h.Tc = (bits>>9)&0x1 == 1
+	h.Rd = (bits>>8)&0x1 == 1
+	h.Ra = (bits>>7)&0x1 == 1
+	h.OpCode = OPCODE((bits >> 11) & 0xF)
+	h.Rcode = RCODE(bits & 0xF)
+	if z := ((bits >> 4) & 0x7); z != 0 {
+		return fmt.Errorf("Z must be 0 and is %d", z)
+	}
 
 	return nil
 }
