5ccfd6c64ff5649c7cd44b8ba4fa894f851bc1cb
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-06-03 21:08:28 +0200

    framework header parsing for dns

--- internal/infrastructure/parser/header_parser.go
+++ internal/infrastructure/parser/header_parser.go
@@ -1,9 +1,14 @@
 package parser
 
+import (
+	"fmt"
+	"io"
+)
+
 type (
-	QR     int
-	OPCODE int
-	RCODE  int
+	QR     uint8
+	OPCODE uint8
+	RCODE  uint8
 )
 
 const (
@@ -42,4 +47,37 @@ type Header struct {
 	OpCode  OPCODE
 	Qr      QR
 	Rcode   RCODE
+}
+
+func CreateHeader() *Header {
+	// quote from rfc 1035
+	// Reserved for future use.  Must be zero in all queries and responses.
+	return &Header{
+		Z: 0,
+	}
+}
+
+func (h *Header) Parse(r io.Reader) error {
+	buff := make([]byte, 12)
+	_, err := io.ReadFull(r, buff)
+	if err != nil {
+		return fmt.Errorf("failed to read into buffer prev: %s", err)
+	}
+	h.Id = [2]byte(buff[:2])
+	h.QdCount = [2]byte(buff[4:6])
+	h.AnCount = [2]byte(buff[6:8])
+	h.NsCount = [2]byte(buff[8:10])
+	h.ArCount = [2]byte(buff[10:])
+
+	miscBuf := [2]byte(buff[2:4])
+	err = h.parseMisc(miscBuf)
+	if err != nil {
+		return fmt.Errorf("failed to parse the second row of 16 bits from dns prev: %s", err)
+	}
+
+	return nil
+}
+
+func (h *Header) parseMisc(b [2]byte) error {
+	return nil
 }
