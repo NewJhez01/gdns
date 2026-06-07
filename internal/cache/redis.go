8bce2fa7494e4460c520d0d1f8cc646d3945d4ac
Owen Harper-Jones <owen.harperjones@gmail.com>
2026-06-07 17:34:59 +0200

    add basic redis logic

---
+++ internal/cache/redis.go
@@ -0,0 +1,33 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	client *redis.Client
+}
+
+func CreateNewRedisClient() *RedisClient {
+	return &RedisClient{}
+}
+
+func (r *RedisClient) GetDomainNameFromCache(ctx context.Context, key string) (string, error) {
+	val, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		return "", fmt.Errorf("failed to fetch domain from cache prev: %s", err)
+	}
+	return val, nil
+}
+
+func (r *RedisClient) SetDomainName(ctx context.Context, key, val string, ttl time.Duration) error {
+	err := r.client.Set(ctx, key, val, ttl)
+	if err != nil {
+		return fmt.Errorf("failed to write domain name: %s into cache prev:%s", val, err)
+	}
+	return nil
+}
